a2e8ac82438b366049a2291bfad4989d0270fba4
MANDANIAINAPRECIEUX <parasybe@gmail.com>
2025-07-02 11:08:37 +0300

    a

---
+++ FRONTEND/src/App.jsx
@@ -0,0 +1,14 @@
+// src/App.jsx
+import React from "react";
+// L'import de './App.css' a été supprimé car le fichier sera vide ou recréé.
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Bienvenue sur l'Agenda Dentaire !</h1>
+      <p>Le frontend est prêt à être développé.</p>
+    </div>
+  );
+}
+
+export default App;
