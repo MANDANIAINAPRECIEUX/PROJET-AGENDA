1726a76cb70092ae5deb856880e1d5f4e971de79
unknown <mandaniainaprecieux@yahoo.com>
2026-02-17 23:27:33 +0300

    efatra

--- BACKEND/models/User.js
+++ BACKEND/models/User.js
@@ -33,7 +33,7 @@ const userSchema = mongoose.Schema(
     prenom: {
       // Correspond à `prenom` dans votre formData du frontend
       type: String,
-      required: [true, "Veuillez ajouter au moins un prénom"],
+      required: [true, "Veuillez ajouter  un prénom"],
       trim: true,
     },
     nom: {
