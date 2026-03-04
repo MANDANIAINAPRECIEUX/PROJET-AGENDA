90b6c3e7eeb335a410d3d6275a074c07ec66cddd
unknown <mandaniainaprecieux@yahoo.com>
2026-03-04 23:33:38 +0300

    y

--- BACKEND/models/User.js
+++ BACKEND/models/User.js
@@ -18,7 +18,7 @@ const userSchema = mongoose.Schema(
     },
     password: {
       type: String,
-      required: [true, "Veuillez ajouter un mot de passe"],xsxqs qsxqxsq qsdqsdqsd
+      required: [true, "Veuillez ajouter un mot de passe"],xsxqs qsxqxsq
       minlength: [
         6,
         "Le mot de passe doit contenir plus six caractères",
