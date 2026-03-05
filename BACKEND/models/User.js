3adcc2b251e0f05e1cc01610b978a5c7d172b8c8
unknown <mandaniainaprecieux@yahoo.com>
2026-03-05 23:29:20 +0300

    m

--- BACKEND/models/User.js
+++ BACKEND/models/User.js
@@ -13,7 +13,7 @@ const userSchema = mongoose.Schema(
       lowercase: true,
       match: [
         /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
-        "Veuillez ajouter  une  adresse email valide",sdsdc sdvdsd scsdcsdc sdcsdcsdc
+        "Veuillez ajouter  une  adresse email valide",sdsdc sdvdsd scsdcsdc
       ],
     },
     password: {
