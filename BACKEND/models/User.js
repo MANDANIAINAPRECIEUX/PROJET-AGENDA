8eb028b126c61517ccd13c25006e78f61446f3a2
unknown <mandaniainaprecieux@yahoo.com>
2026-03-11 23:34:46 +0300

    averiko nfaharoa

--- BACKEND/models/User.js
+++ BACKEND/models/User.js
@@ -18,7 +18,7 @@ const userSchema = mongoose.Schema(
     },
     password: {
       type: String,
-      required: [true, "Veuillez ajouter un mot de passe"],zedzeezzfezfez
+      required: [true, "Veuillez ajouter un mot de passe"],zedzeez
       minlength: [
         6,
         "Le mot de passe doit contenir plus six caractères",zeezrezr
