7c4f1c0c2fbed4846aa46538294a92c6009f5b1d
unknown <mandaniainaprecieux@yahoo.com>
2026-03-12 23:48:06 +0300

    erreur 3: mba jereo ko le assets

--- BACKEND/models/User.js
+++ BACKEND/models/User.js
@@ -22,7 +22,7 @@ const userSchema = mongoose.Schema(
       minlength: [
         6,
         "Le mot de passe doit contenir plus six caractères",
-      ],dsfsdfs fsdf sdfdfsf  sdf sdf
+      ],dsfsdfs fsdf sdfdfsf  sdf
       select: false, 
     },
     role: {
