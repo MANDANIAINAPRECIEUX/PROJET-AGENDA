c1ff123fda502a4146a84bfee84d2ca8cabf65bf
unknown <mandaniainaprecieux@yahoo.com>
2026-03-12 23:46:43 +0300

    all thing mandeha 1

--- BACKEND/models/User.js
+++ BACKEND/models/User.js
@@ -22,7 +22,7 @@ const userSchema = mongoose.Schema(
       minlength: [
         6,
         "Le mot de passe doit contenir plus six caractères",
-      ],
+      ],dsfsdfs fsdf sdfdfsf  sdf sdf sdf sdsf sf sfs
       select: false, 
     },
     role: {
