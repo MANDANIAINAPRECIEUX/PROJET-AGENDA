b2c93ea35db3d51311e707723f5df6e745788908
unknown <mandaniainaprecieux@yahoo.com>
2026-03-17 23:45:35 +0300

    mada1

--- BACKEND/models/User.js
+++ BACKEND/models/User.js
@@ -24,7 +24,7 @@ const userSchema = mongoose.Schema(
         "Le mot de passe doit contenir plus six caractères",
       ],
       select: false, 
-    },
+    },dqsdqsd sqdqs qs dqs qsd qd qdqs qsd
     role: {
       type: String,
       enum: ["admin", "dentiste", "patient"],
