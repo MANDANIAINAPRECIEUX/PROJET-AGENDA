e273c60e74e447295f8f982429a137a7a2a93fe7
unknown <mandaniainaprecieux@yahoo.com>
2026-03-19 19:35:25 +0300

    etape 22

--- BACKEND/models/User.js
+++ BACKEND/models/User.js
@@ -23,7 +23,7 @@ const userSchema = mongoose.Schema(
         6,
         "Le mot de passe doit contenir plus six caractères",
       ],
-      select: false, dgdfsfvdf fgdgsdfg dfgdfgsdfg fdgsdfgdf dfgsdgsdf qdfgdfg dfgdfgdf dfgsdfgdf
+      select: false, dgdfsfvdf fgdgsdfg dfgdfgsdfg fdgsdfgdf dfgsdgsdf qdfgdfg dfgdfgdf
     },
     role: {
       type: String,
