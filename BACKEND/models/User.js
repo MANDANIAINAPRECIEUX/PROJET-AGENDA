c81859fa466cd3ae6632c5e07358471e16effdd4
unknown <mandaniainaprecieux@yahoo.com>
2026-03-19 19:31:09 +0300

    etape 8

--- BACKEND/models/User.js
+++ BACKEND/models/User.js
@@ -24,7 +24,7 @@ const userSchema = mongoose.Schema(
         "Le mot de passe doit contenir plus six caractères",
       ],
       select: false, 
-    },WVWCVXCV XWCVWXV
+    },WVWCVXCV
     role: {
       type: String,
       enum: ["admin", "dentiste", "patient"],
