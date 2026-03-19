dd770f6d63d40d6c8ec84cd2b894f3b812fe7904
unknown <mandaniainaprecieux@yahoo.com>
2026-03-19 19:37:22 +0300

    farany

--- BACKEND/models/User.js
+++ BACKEND/models/User.js
@@ -23,7 +23,7 @@ const userSchema = mongoose.Schema(
         6,
         "Le mot de passe doit contenir plus six caractères",
       ],
-      select: false, dgdfsfvdf
+      select: false, 
     },
     role: {
       type: String,
