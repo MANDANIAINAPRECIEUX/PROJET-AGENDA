3aed3953af074e6f704dd9e62bc1e4e07278b374
unknown <mandaniainaprecieux@yahoo.com>
2026-03-21 23:49:51 +0300

    dingana hafa

--- BACKEND/models/User.js
+++ BACKEND/models/User.js
@@ -24,7 +24,7 @@ const userSchema = mongoose.Schema(
         "Le mot de passe doit contenir plus six (6) caractères",
       ],
       select: false, 
-    },sfd  sdf qsdf dfqs sdf
+    },sfd  sdf qsdf dfqs
     role: {
       type: String,
       enum: ["admin", "dentiste", "patient"],
