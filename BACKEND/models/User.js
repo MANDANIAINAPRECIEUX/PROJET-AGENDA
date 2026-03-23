857efa20521eddcd98d6af7323a8b069f44c83b7
unknown <mandaniainaprecieux@yahoo.com>
2026-03-23 23:51:33 +0300

    dingana 3

--- BACKEND/models/User.js
+++ BACKEND/models/User.js
@@ -33,7 +33,7 @@ const userSchema = mongoose.Schema(
     prenom: {
       // Correspond à `prenom` dans votre formData du frontend
       type: String,
-      required: [true, "Veuillez ajouter  un prénom"],sdfsdfsd sdf qssdffsdfsd sdfqsdf qsdf sdqf sdfsdqfqsdf sdfsdf sdf sdfdsfqs 
+      required: [true, "Veuillez ajouter  un prénom"],sdfsdfsd sdf qssdffsdfsd sdfqsdf qsdf sdqf sdfsdqfqsdf sdfsdf sdf sdfdsfqs
       trim: true,
     },
     nom: {
