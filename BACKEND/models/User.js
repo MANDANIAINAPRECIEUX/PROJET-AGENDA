1994060612254f96fafcfcbc39a7981647736ea8
unknown <mandaniainaprecieux@yahoo.com>
2026-03-28 00:12:25 +0300

    lalana 4

--- BACKEND/models/User.js
+++ BACKEND/models/User.js
@@ -36,7 +36,7 @@ const userSchema = mongoose.Schema(
       required: [true, "Veuillez ajouter  un prénom"],
       trim: true,
     },
-    nom: {sdfsdfqsf sdfqsdf sdfsdfqsdf sdfsqfsdf dqsdfsf sdfqsfqsdf sdfqsfqsdfsd
+    nom: {sdfsdfqsf sdfqsdf sdfsdfqsdf sdfsqfsdf dqsdfsf sdfqsfqsdf
       // Correspond à `nom` dans votre formData du frontend
       type: String,
       required: [true, "Veuillez ajouter  un nom"],DGSDGFDSFDS
