10e82fedf6f1dac83df1c92d72005bed31489d55
unknown <mandaniainaprecieux@yahoo.com>
2026-03-29 23:31:17 +0300

    finish

--- BACKEND/models/User.js
+++ BACKEND/models/User.js
@@ -39,7 +39,7 @@ const userSchema = mongoose.Schema(
     nom: {
       // Correspond à `nom` dans votre formData du frontend
       type: String,
-      required: [true, "Veuillez ajouter  un nom"],sdgdfsgdf
+      required: [true, "Veuillez ajouter  un nom"],
       trim: true,
     },
     telephone: {
