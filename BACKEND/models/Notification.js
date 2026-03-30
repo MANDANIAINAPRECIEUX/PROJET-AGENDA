60af54a3a6f1f435851cdc39f2989608b9eee511
unknown <mandaniainaprecieux@yahoo.com>
2026-03-30 22:51:19 +0300

    fahasivy

--- BACKEND/models/Notification.js
+++ BACKEND/models/Notification.js
@@ -5,12 +5,7 @@ const notificationSchema = mongoose.Schema({
     recipient: {
         type: mongoose.Schema.Types.ObjectId,
         required: true,
-        // Pour le moment, nous allons permettre de référencer soit un Patient, soit un Dentiste.
-        // Une approche plus avancée pourrait utiliser un champ 'onModel' pour une référence dynamique
-        // ou un modèle 'User' général pour centraliser les destinataires.
-        // Pour l'instant, on suppose que recipient pourrait être un patient ou un dentiste.
-        // On le précisera dans les contrôleurs.
-        // Si nous avions un modèle 'User', ce serait 'ref: 'User''
+        
     },
     recipientModel: { // Champ pour indiquer si le destinataire est un Patient ou un Dentist
         type: String,
@@ -31,12 +26,7 @@ const notificationSchema = mongoose.Schema({
         type: Boolean,
         default: false
     },
-    // Vous pouvez ajouter une référence au rendez-vous si la notification est liée à un RDV
-    // rendezVous: {
-    //     type: mongoose.Schema.Types.ObjectId,
-    //     ref: 'RendezVous',
-    //     required: false // Optionnel
-    // }
+    
 }, {
     timestamps: true // Ajoute automatiquement les champs 'createdAt' et 'updatedAt'
 });
