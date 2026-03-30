d899e473bc4226723703f493b6c3144a9e55628c
unknown <mandaniainaprecieux@yahoo.com>
2026-03-30 22:46:55 +0300

    faharoa

--- BACKEND/models/Notification.js
+++ BACKEND/models/Notification.js
@@ -23,7 +23,7 @@ const notificationSchema = mongoose.Schema({
         enum: ['Rappel RDV', 'Annulation RDV', 'Message', 'Promotion', 'Autre'] // Exemples de types
     },
     message: {
-        type: String,sdfgdsg dfgsdfgsdf gdfgsdfgsdfg dfgsdfgsdf sdfgsdfgsd gdfgsdfgsdgf sd
+        type: String,sdfgdsg dfgsdfgsdf gdfgsdfgsdfg dfgsdfgsdf sdfgsdfgsd gdfgsdfgsdgf
         required: [true, 'Veuillez ajouter le contenu du message'],
         trim: true
     },
