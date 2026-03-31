8908cc4904684fcd1e529a7fe4bbc8348ccfae73
unknown <mandaniainaprecieux@yahoo.com>
2026-03-31 21:29:23 +0300

    mety8

--- BACKEND/models/Confirmation.js
+++ BACKEND/models/Confirmation.js
@@ -13,7 +13,7 @@ const confirmationSchema = mongoose.Schema({
         ref: 'Dentist' // Fait référence au modèle 'Dentist'
     },
     dateAction: { // Mappe à dateAction
-        type: Date,dgdgsdg
+        type: Date,
         required: [true, 'Veuillez spécifier la date de l\'action de confirmation'],
         default: Date.now // Définit la date actuelle par défaut si non spécifiée
     },
