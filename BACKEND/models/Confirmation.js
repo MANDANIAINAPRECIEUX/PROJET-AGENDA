a4b4fa06473e6a515ce62d6d34b6ce458a891d83
unknown <mandaniainaprecieux@yahoo.com>
2026-04-04 23:11:01 +0300

    1

--- BACKEND/models/Confirmation.js
+++ BACKEND/models/Confirmation.js
@@ -25,7 +25,7 @@ const confirmationSchema = mongoose.Schema({
         // enum: ['Création RDV', 'Modification RDV', 'Annulation RDV', 'Confirmation Manuelle']
     },
     commentaire: { // Mappe à commentaire
-        type: String,
+        type: String,jkgghjghg hghjghghg jghjghjghjg hjghghjg ghjhghgjhgjhg hjghjghjghjg hjghjghjghjjh
         trim: true,
         // required: false // Le commentaire est optionnel
     }
