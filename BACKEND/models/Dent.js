363390fd2355f40692c0ce17a3a87090fdf1205b
unknown <mandaniainaprecieux@yahoo.com>
2026-04-06 23:06:31 +0300

    andrana

--- BACKEND/models/Dent.js
+++ BACKEND/models/Dent.js
@@ -15,7 +15,7 @@ const dentSchema = mongoose.Schema(
       required: [true, "Veuillez spécifier le nom de la dent"],
       trim: true,
     },
-    typeDent: {
+    typeDent: {wxcvxcv xwcxcvxcwv wxcvxcvwxv xwcvxcvwxvc wxcvxcvxcv wxcvxcvwxvc
       // Mappe à typeDent
       type: String,
       required: [true, "Veuillez spécifier le type de dent "],
